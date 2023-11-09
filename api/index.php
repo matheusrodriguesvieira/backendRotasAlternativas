7b02db16630867a83cec72a8288659fc5798a1c6
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2023-11-09 20:42:46 -0300

    teste de novo sistema de rotas

---
+++ api/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// Roteamento manual
+$uri = isset($_SERVER['REQUEST_URI']) ? parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) : '/';
+
+switch ($uri) {
+    case '/':
+    case '/home':
+        require_once 'controllers/HomeController.php';
+        $controller = new HomeController();
+        $controller->index();
+        break;
+    case '/about':
+        require_once 'controllers/AboutController.php';
+        $controller = new AboutController();
+        $controller->index();
+        break;
+    default:
+        // Lidar com 404
+        http_response_code(404);
+        echo '404 Not Foung';
+        break;
+}
