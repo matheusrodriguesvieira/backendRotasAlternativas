95751c2169f347fbd56901e095d6339afb1c0589
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2023-11-09 20:57:08 -0300

    teste 3 corrigido

--- api/index.php
+++ api/index.php
@@ -28,33 +28,3 @@ include_once './database/DB.php';
 include_once './controllers/listaEscalas.php';
 include_once './controllers/operadoresController.php';
 include_once './controllers/equipamentosCotroller.php';
-
-
-
-include_once './database/DB.php';
-
-switch ($uri) {
-    case '/':
-        break;
-    case '/equipamentos':
-        require_once 'controllers/HomeController.php';
-        // $controller = new HomeController();
-        $controller->index();
-        break;
-    case '/operadores':
-        require_once 'controllers/AboutController.php';
-        // $controller = new AboutController();
-        $controller->index();
-        break;
-    case '/lista-escalas':
-        require_once 'controllers/AboutController.php';
-        // $controller = new AboutController();
-        $controller->index();
-        break;
-    default:
-        // Lidar com 404
-        http_response_code(404);
-        echo '404 Not Foung';
-        break;
-}
-exit;
