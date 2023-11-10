1e7fbc97dd16862e555730adb111f181bfc7979d
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2023-11-10 10:52:59 -0300

    atualizaçao do arquivo de conexao

--- api/database/DB.php
+++ api/database/DB.php
@@ -9,7 +9,8 @@ class DB
         $user = 'matheusrodriguespgm';
         $pass = 'Vb8yEXQAOvf9';
         $sslmode = 'require';
+        $options = 'endpoint=ep-fancy-haze-53308299';
 
-        return new PDO("pgsql:host=$host;dbname=$base;user=$user;password=$pass;sslmode=$sslmode?options=endpoint%3Dep-fancy-haze-53308299");
+        return new PDO("pgsql:host=$host;dbname=$base;user=$user;password=$pass;sslmode=$sslmode;options=$options");
     }
 }
